39443f13377c1fe6a0b900dbe02557cbb3b1e6cf
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-23 04:01:15 +0500

    Code_Sample_09

---
+++ Code Sample 09 Simple Method/Code Sample 09 Simple Method/Code Sample 09 Simple Method/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Sample_09_Simple_Method
+{
+    class MethDemo
+    {
+        static void doit()
+        {
+            Console.WriteLine("Hello");
+        }
+        static void Main(string[] args)
+        {
+            doit();
+            doit();
+            Console.ReadKey();
+        }
+    }
+}
