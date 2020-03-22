a57bda93f2f77182ffe8bf680e1c2ea7c5df3af9
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-23 02:27:56 +0500

    Code_Sample_06 WhileLoopInsteadOfFor

---
+++ Code Sample 06 While Loop instead of For/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Sample_06 
+{
+    class  WhileLoopInsteadOfFor
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            i = 1;
+            while(i <= 10)
+            {
+                Console.WriteLine(i);
+
+                i = i + 1;
+            }
+            Console.ReadKey();
+        }
+    }
+}
