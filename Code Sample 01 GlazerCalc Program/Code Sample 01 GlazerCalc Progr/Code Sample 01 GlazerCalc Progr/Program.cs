927944303bf2debad6b367e26e77e14a953dd7a9
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-22 23:09:01 +0500

    Code_Sample_01

---
+++ Code Sample 01 GlazerCalc Program/Code Sample 01 GlazerCalc Progr/Code Sample 01 GlazerCalc Progr/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Sample_01_GlazerCalc_Progr
+{
+    class GlazerCalc
+    {
+        static void Main(string[] args)
+        {
+            Double width, height, woodLength, glassArea;
+            string widthString, heightString;
+
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+
+            woodLength = 2 * (width + height) * 3.25;
+
+            glassArea = 2 * (width * height);
+
+            Console.WriteLine("The length of wood is " + woodLength + "feet");
+            Console.WriteLine("The area of the glass is " + glassArea + " square meters");
+            Console.ReadKey();
+        }
+    }
+}
