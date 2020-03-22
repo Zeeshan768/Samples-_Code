1253ae72b8cb0440f963e4e63c3a116951fcab6b
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-23 03:07:57 +0500

    Code_Sample_07_For_loop

---
+++ Code Sample 07 For loop/Code Sample 07 For loop/Code Sample 07 For loop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Sample_07_For_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for(i = 1;i < 11; i=i+1)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
