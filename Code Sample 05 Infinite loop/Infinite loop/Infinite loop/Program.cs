fa0644ded63e96382f246f2c4b5fba1e91b3843c
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-23 02:10:46 +0500

    Infinite_loop

---
+++ Code Sample 05 Infinite loop/Infinite loop/Infinite loop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infinite_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Hello num");
+            }
+            while (true);
+        }
+    }
+}
