532d0d42b1d323a135c246a5ab83fb6d9207d474
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-24 04:02:32 +0500

    Sample Code 16

---
+++ Code Sample 16 Using an Array/Sample Code 16/Sample Code 16/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Sample_Code_16
+    {
+    class Program
+        {
+        static string readString(string promp)
+            {
+            string result;
+            do
+                {
+                Console.Write(promp);
+                result = Console.ReadLine();
+                } while(result == "");
+            return result;
+            }
+        static int readInt(string promp,int low,int high)
+            {
+            int result;
+            do
+                {
+                string intString = readString(promp);
+                result = int.Parse(intString);
+                } while((result < low) || (result > high));
+            
+        return result;
+            }
+        static void Main(string[] args)
+            {
+            const int SCORE_SIZE = 0;
+            int [] score = new int[SCORE_SIZE];
+            for(int i = 0; i < SCORE_SIZE; i = i +1)
+                {
+                score[i] = readInt("Score : ", 0, 1000);
+                }
+            for (int i = 0; i < SCORE_SIZE ; i = i +1)
+                {
+                Console.WriteLine( "Score: " + score[i]);
+                }Console.ReadKey();
+            }
+        }
+    }
