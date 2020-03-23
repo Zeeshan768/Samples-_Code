bad068b5c59bcbbff84df8f0b81905a86bac7886
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-24 02:45:47 +0500

    Code Sample 15

---
+++ Code Sample 15 Using a member variable/Sample Code 15/Sample Code 15/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Sample_Code_15
+    {
+    class Program
+        {
+        static int member = 0;
+        static void Othermethod()
+            {
+            member = 99;
+            }
+        static void Main()
+            {
+            Console.WriteLine("member is : " + member);
+            Othermethod();
+            Console.WriteLine("member is now : " + member);
+            Console.ReadKey();
+            }
+        }
+    }
