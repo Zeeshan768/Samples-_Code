9e06490d68a64f52d27b7e65114dd604e8753a47
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-25 04:03:59 +0500

    Code Sample 26

---
+++ Code Sample 26 Simple Account Stucture/Code Sample 26/Code Sample 26/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Code_Sample_26
+    {
+    struct AccountStruct
+        {
+        public string Name;
+        };
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            AccountStruct RobsAccountStruct;
+            RobsAccountStruct.Name = "Rob";
+            Console.WriteLine(RobsAccountStruct.Name);
+            Console.ReadKey();
+            }
+        }
+    }
