c7985e8002b9633481b5caabe840cb6cf130d57b
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-25 02:03:49 +0500

    Code Sample 22

---
+++ Code Sample 22 Enum Traffic Light/Code Sample 22/Code Sample 22/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Code_Sample_22
+    {
+    enum TrafficLight
+        {
+        Red,
+        RedAmber,
+        Green,
+        Amber
+        };
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            TrafficLight light;
+            light = TrafficLight.Red;
+            Console.WriteLine(light);
+            Console.ReadKey();
+            }
+        }
+    }
