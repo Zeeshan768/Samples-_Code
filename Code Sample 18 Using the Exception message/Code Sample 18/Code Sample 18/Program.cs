8f7cbe980dd98e6a2461a7bd6c0da291e1c018d3
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-24 08:05:35 +0500

    Code Sample 18

---
+++ Code Sample 18 Using the Exception message/Code Sample 18/Code Sample 18/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Code_Sample_17
+    {
+    class Program
+        {
+         public static void Main()
+            {
+            int age;
+            Console.Write("Enater your age: ");
+            string ageString = Console.ReadLine();
+            try
+            {
+                age = int.Parse(ageString);
+                Console.WriteLine("Thank you ");
+             }
+            catch(Exception e)
+                {
+                Console.WriteLine(e.Message);
+                }Console.Read();
+            }
+        }
+    }
