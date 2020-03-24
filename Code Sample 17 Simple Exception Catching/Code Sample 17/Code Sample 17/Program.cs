b4fd5b07707d0270955c151f0c529a0a86f9302c
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-24 07:48:41 +0500

    Code Sample 17

---
+++ Code Sample 17 Simple Exception Catching/Code Sample 17/Code Sample 17/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Code_Sample_17
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            int age;
+            Console.Write("Enater your age: ");
+            string ageString = Console.ReadLine();
+            try
+            {
+                age = int.Parse(ageString);
+                Console.WriteLine("Thank you ");
+             }
+            catch
+                {
+                Console.WriteLine("Invalid age value");
+                }Console.Read();
+            }
+        }
+    }
