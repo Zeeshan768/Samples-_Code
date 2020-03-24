16f608375661320cb92532e2df5972a97179c9d7
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-25 03:43:31 +0500

    Code Sample 25

---
+++ Code Sample 25 Printing Account values from an array/Code Sample 25/Code Sample 25/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Code_Sample_23
+    {
+    enum AccountState
+        {
+        New,
+        Active,
+        UnderAudit,
+        Frozen,
+        Closed
+        };
+    struct Account
+        {
+        public AccountState State;
+        public string Name;
+        public string Address;
+        public int AccountNumber;
+        public int Balance;
+        public int Overdraft;
+        };
+    class Program
+        {
+        public static void PrintAccount(Account a)
+            {
+            Console.WriteLine("Name :" + a.Name);
+            Console.WriteLine("Address :" + a.Address);
+            Console.WriteLine("Balance :" + a.Balance);
+            }
+        static void Main(string[] args)
+            {
+            const int MAX_CUST = 100;
+            Account[] Bank = new Account [MAX_CUST];
+            Bank[0].Name = "Rob";
+            Bank[0].State = AccountState.Active;
+            Bank[0].Balance = 1000000;
+            PrintAccount(Bank[0]);
+            Bank[1].Name = "jim";
+            Bank[1].State = AccountState.Frozen;
+            Bank[1].Balance = 0;
+            Console.WriteLine(Bank[0].Name+" "+Bank[1].State);
+            PrintAccount(Bank[0]);
+           Console.ReadKey();
+            }
+        }
+    }
