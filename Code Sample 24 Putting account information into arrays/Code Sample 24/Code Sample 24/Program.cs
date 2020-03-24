766c7683b250062cb47372cedb13f7bf72310610
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-25 03:01:17 +0500

    Code Sample 24

---
+++ Code Sample 24 Putting account information into arrays/Code Sample 24/Code Sample 24/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Code_Sample_23
+    {
+    enum AccountState
+        {
+        New,
+        Active,
+        UnderAudit,
+        Frozen,
+        Closed
+        };
+    struct Account
+        {
+        public AccountState State;
+        public string Name;
+        public string Address;
+        public int AccountNumber;
+        public int Balance;
+        public int Overdraft;
+        };
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            const int MAX_CUST = 100;
+            Account[] Bank = new Account [MAX_CUST];
+            Bank[0].Name = "Rob";
+            Bank[0].State = AccountState.Active;
+            Bank[0].Balance = 1000000;
+            Bank[1].Name = "jim";
+            Bank[1].State = AccountState.Frozen;
+            Bank[1].Balance = 0;
+            Console.WriteLine(Bank[0].Name+" "+Bank[1].State);
+           Console.ReadKey();
+            }
+        }
+    }
