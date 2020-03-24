3681a302fab19cd5daa6b5da1ab6f5ac5d4bdc45
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-25 00:28:31 +0500

    Sample Code 21

---
+++ Code Sample 21 File Write and Read/Code Sample 21/Code Sample 21/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+namespace Code_Sample_21
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            StreamWriter writer;
+            writer = new StreamWriter("text.text");
+            writer.WriteLine("hello world");
+            writer.Close();
+
+            StreamReader reader = new StreamReader("text.text");
+            while(reader.EndOfStream == false)
+                {
+                string line = reader.ReadLine();
+                Console.WriteLine(line);
+                }
+            reader.Close();
+            Console.ReadKey();
+            }
+        }
+    }
