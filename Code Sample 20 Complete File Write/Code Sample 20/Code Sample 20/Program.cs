a06bc3cdbe9355161574f62c1b42de6a303f240c
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-24 20:05:45 +0500

    Code Sample 20

---
+++ Code Sample 20 Complete File Write/Code Sample 20/Code Sample 20/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+namespace Code_Sample_20
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            StreamWriter writer;
+            writer = new StreamWriter("text.text");
+            writer.WriteLine("hello world");
+            writer.Close();
+            }
+        }
+    }
