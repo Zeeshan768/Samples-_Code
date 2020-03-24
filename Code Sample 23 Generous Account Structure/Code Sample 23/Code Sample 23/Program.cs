5a059e75b5a1fc56aa30545132f7dd75bb19b34a
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-25 02:38:15 +0500

    Code Sample 23

---
+++ Code Sample 23 Generous Account Structure/Code Sample 23/Code Sample 23/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Code_Sample_23
+    {
+    enum AccountState {New, Active, UnderAudit,
+        Frozen, Closed };
+    struct Account
+        {
+        public AccountState State;
+        public string Name;
+        public string Address;
+        public int AccountNumber;
+        public int Balance;
+        public int Overdraft;
+        };
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            Account RobsAccount;
+            RobsAccount.State = AccountState.Active;
+            RobsAccount.Name = "Zeeshan";
+            RobsAccount.Balance = 1000000;
+            Console.WriteLine(RobsAccount.State+"   "+RobsAccount.Balance + "  "+RobsAccount.Name);
+            Console.ReadKey(); 
+            }
+        }
+    }
