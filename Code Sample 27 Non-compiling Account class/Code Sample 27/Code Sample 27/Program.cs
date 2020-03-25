591d20ece1e2face43456fccb2dde1d9ca590f96
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-25 23:12:32 +0500

    Code Sample 27

---
+++ Code Sample 27 Non-compiling Account class/Code Sample 27/Code Sample 27/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Code_Sample_27
+    {
+    class Account
+        {
+        public string Name;
+        }
+    class StructAndObjectDemo
+        {
+        public static void Main(string[] args)
+            {
+            Account RobsAccount = new Account();
+            RobsAccount.Name = "Rob";
+            Console.WriteLine(RobsAccount.Name);
+            Console.ReadKey();
+            }
+        }
+    }
