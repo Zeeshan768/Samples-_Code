57b1ae3ca8893de471e14b724cdd73f82350d629
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-26 04:36:45 +0500

    Code Sample 34

---
+++ Code Sample 34 Using a custom constructor/Code Sample 34/Code Sample 34/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Code_Sample_34
+    {
+    class Account
+        {
+        private string name;
+        private string address;
+        private decimal balance;
+        public  Account(string inName,string inAddress,decimal inBalance)
+            {
+            name = inName;
+            address = inAddress;
+            balance = inBalance;
+            }
+        }
+    class Bank
+        {
+        static void Main(string[] args)
+            {
+            Account robAccount;
+            robAccount = new Account("Rob","Robs House",100000);
+            }
+        }
+    }
