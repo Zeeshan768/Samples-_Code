d05709cd432acbc2d4792079738f365c071d917e
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-26 01:42:08 +0500

    Code Sample 31

---
+++ Code Sample 31 Withdraw insufficient funds/Code Sample 31/Code Sample 31/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Code_Sample_31
+    {
+    class Account
+        {
+        private decimal balance = 0;
+        public bool WithdrawFunds(decimal amount)
+            {
+            if(balance < amount)
+                {
+                return false;
+                }
+            balance = balance - amount;
+            return true;
+            }
+        };
+    class Bank
+        {
+        static void Main(string[] args)
+            {
+            Account RobsAccount;
+            RobsAccount = new Account();
+            if(RobsAccount.WithdrawFunds(5))
+                {
+                Console.WriteLine("Cash Withdraw");
+                }
+            else
+                {
+                Console.WriteLine("Insufficient Funds");
+                }
+            }
+        }
+    }
