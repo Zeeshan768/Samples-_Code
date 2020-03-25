d96c66d7c3fc297022d4477156bec8f320ef4b24
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-26 00:24:50 +0500

    Code Sample 30

---
+++ Code Sample 30 No references to an instance/Code Sample 30/Code Sample 30/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Code_Sample_30
+    {
+    class Account
+        {
+        public string Name;
+        }
+    class StructAndObjectDemo
+        {
+        public static void Main(string[] args)
+            {
+            Account RobsAccount;
+            RobsAccount = new Account();
+            RobsAccount.Name = "Rob";
+            Console.WriteLine(RobsAccount.Name);
+            RobsAccount = new Account();
+            RobsAccount.Name = "Jim";
+            Console.WriteLine(RobsAccount.Name);
+            Console.ReadKey();
+            }
+        }
+    }
