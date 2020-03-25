6a4831741464f59c9d4546a9aad9e87009c99941
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-26 02:34:55 +0500

    Cose Sample 32

---
+++ Code Sample 32 Testing the Account Class/Code Sample 32/Code Sample 32/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Code_Sample_32
+    {
+    class Account
+        {
+        private decimal balance = 0;
+        public bool WithdrawFunds(decimal amount)
+            {
+            if(balance < amount)
+                {
+                return false;
+                }
+            balance = balance - amount;
+            return true;
+            }
+
+        public void PayIntFunds(decimal amount)
+            {
+            balance = balance + amount;
+            }
+        public decimal GetBalance()
+            {
+            return balance;
+            }
+        }
+    class Bank
+        {
+        public static void Main(string[] args)
+            {
+            Account test = new Account();
+            test.PayIntFunds(50);
+            if(test.GetBalance() != 50)
+                {
+                Console.WriteLine("Pay in test failed");
+                }
+            else
+                {
+                Console.WriteLine("Pay In test succeeded");
+                }
+            Console.ReadKey();
+            }
+        }
+    }
