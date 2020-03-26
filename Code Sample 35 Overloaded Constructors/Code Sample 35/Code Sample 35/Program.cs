9e32ee4da27e850ceae7dfca127169f93315e181
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-26 07:45:35 +0500

    Code Sample 35

---
+++ Code Sample 35 Overloaded Constructors/Code Sample 35/Code Sample 35/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Code_Sample_34
+    {
+    class Account
+        {
+        private string name;
+        private string address;
+        private decimal balance;
+        public  Account(string inName,string inAddress,decimal inBalance)
+            {
+            name = inName;
+            address = inAddress;
+            balance = inBalance;
+            }
+        public Account(string inName, string inAddress) :
+            this(inName, inAddress, 0)
+            {
+            }
+            public Account (string inName) :
+            this (inName, "Not Supplied",0)
+            {
+
+            }
+            
+        }
+    class Bank
+        {
+        static void Main(string[] args)
+            {
+            const int MAX_CUST = 100;
+            Account [] Accounts = new Account[MAX_CUST];
+            Accounts[0] = new Account("Rob", "Robs House", 100000);
+            Accounts[1] = new Account("jim", "Jims House");
+            Accounts[2] = new Account("Fred");
+            }
+        }
+    }
