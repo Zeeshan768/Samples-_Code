3012475ec6f210a53c4d475bcc115b724e851d41
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-28 01:39:45 +0500

    Code sample 41

---
+++ Code Sample 41 Using an Abstract method/Code Sample 41/Code Sample 41/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+namespace Code_Sample_41
+    {
+    public interface IAccount
+        {
+        void PayInFunds(decimal amount);
+        bool WithdrawFunds(decimal amount);
+        decimal GetBalance();
+        string RudeLetterString();
+        }
+    public abstract class Account:IAccount
+        {
+        private decimal balance = 0;
+        public abstract string RudeLetterString();
+        public virtual bool WithdrawFunds(decimal amount)
+            {
+            if(balance < amount)
+                {
+                return false;
+                }
+            balance = balance - amount;
+            return true;
+            }
+        public decimal GetBalance()
+            {
+            return balance;
+            }
+        public void PayInFunds(decimal amount)
+            {
+            balance = balance + amount;
+            }
+
+        } 
+    public class CustomerAccount : Account
+        {
+        public override string RudeLetterString()
+            {
+            return "You are overdrawn";
+            }
+        }
+    public class BabyAccount :Account
+        {
+        public override bool WithdrawFunds(decimal amount)
+            {
+            if(amount > 10)
+                {
+                return false;
+                }
+            return base.WithdrawFunds(amount);
+            }
+        public override string RudeLetterString()
+            {
+            return "Tell daddy you are overdrawn";
+            }
+        }
+   
+            class Bank
+        {
+        const int MAX_CUST = 100;
+        public static void Main()
+            {
+            IAccount[] accounts = new IAccount[MAX_CUST];
+            accounts[0] = new CustomerAccount();
+            accounts[0].PayInFunds(5);
+            Console.WriteLine("Balance: " + accounts[0].RudeLetterString());
+
+            accounts[1] = new BabyAccount();
+            Console.WriteLine("Balance: " + accounts[1].RudeLetterString());
+            Console.ReadKey();
+            }
+        }
+    }
