dd2d98d7f882065a824f68838c454fd616fc0970
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-28 03:35:22 +0500

    Code Sample 42

---
+++ Code Sample 42 ToString method/Code Sample 42/Code Sample 42/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Code_Sample_42
+    {
+    class Account
+        {
+        private string name;
+        private decimal balance;
+        public override string ToString()
+            {
+            return "Name: " + name + "balance: " + balance;
+            }
+        public Account(string inName,decimal inBalance)
+            {
+            name = inName;
+            balance = inBalance;
+            }
+        }
+    class bhance 
+        {
+        private string name;
+        private decimal balance;
+        public override string ToString()
+            {
+          return  "balance: " + balance + "Name: " + name ;
+            }
+         public bhance(string inName,decimal inBalance)
+            {
+            name = inName;
+            balance = inBalance;
+            }
+        }
+    class Bank
+        {
+        static void Main()
+            {
+            Account a = new Account("Rob",25);
+            Console.WriteLine(a);
+            bhance b = new bhance("jim",22);
+            Console.WriteLine(a);
+            Console.ReadKey();
+            }
+        }
+    }
