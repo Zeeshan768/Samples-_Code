5c9315c253aaa935d94cc3489a8965d6eeed6f88
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-27 10:24:23 +0500

    Code Sample 38

---
+++ Code Sample 38 Using Components/Code Sample 38/Code Sample 38/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Code_Sample_37
+    {
+    public interface IAccount
+        {
+        void PayInFunds(decimal amount);
+        bool WithdrawFunds(decimal amount);
+        decimal GetBalance();
+        }
+    public class CustomerAccount : IAccount
+        {
+        private decimal balance = 0;
+        public bool WithdrawFunds(decimal amount)
+            {
+            if(balance < amount)
+                {
+                return false;
+                }
+            balance = balance - amount;
+            return true;
+            }
+
+        public decimal GetBalance()
+            {
+            return balance;
+            }
+        }
+    class Bankl
+        {
+        static void Main(string[] args)
+            {
+            IAccount account = new CustomerAccount();
+            account.PayInFunds(50);
+            Console.WriteLine("Balance: " + account.GetBalance());
+            }
+        }
+    }
