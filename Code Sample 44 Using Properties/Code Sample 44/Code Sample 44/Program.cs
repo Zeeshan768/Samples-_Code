d14ce7b407c0fb89dbc1d9fb6e2c7f877d13f441
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-29 03:14:50 +0500

    Code Sample 44

---
+++ Code Sample 44 Using Properties/Code Sample 44/Code Sample 44/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Sample_44
+    {
+    public class StaffMember
+        {
+        private int ageValue;
+        public int Age
+            {
+            set
+                {
+                if((value>0)&&(value < 120))
+                    {
+                    this.ageValue = value;
+                    }
+                }
+            get
+                {
+                return this.ageValue;
+                }
+            }
+        }
+   public  class StaffDemo
+        {
+        static void Main()
+            {
+            StaffMember s = new StaffMember();
+            s.Age = 21;
+            Console.WriteLine("Age is : " + s.Age);
+            Console.ReadKey();
+            }
+        }
+    }
