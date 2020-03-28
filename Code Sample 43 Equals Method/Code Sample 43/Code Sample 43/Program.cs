930bd0ed9ecfd7f4adc2815154a6f8f88a8dc148
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-28 05:31:04 +0500

    Code Smple 43

---
+++ Code Sample 43 Equals Method/Code Sample 43/Code Sample 43/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Code_Sample_43
+    {
+    class Point
+        {
+        public int x;
+        public int y;
+        public override bool Equals(object obj)
+            {
+            Point p = (Point)obj;
+            if((p.x == x) && (p.y == y))
+                {
+                return true;
+                }
+            else
+                {
+                return false;
+                }
+            }
+        public Point(int inX,int inY)
+            {
+            x = inX;
+            y = inY;
+            }
+        }
+    class VideoGame
+        {
+        public static void Main(string[] args)
+            {
+            Point spaceshipPosition = new Point(10,10);
+            Point missilePosition = new Point(10,10);
+            if(missilePosition.Equals(spaceshipPosition))
+                {
+                Console.WriteLine("Bang");
+                }
+
+            Console.ReadKey();
+            }
+        }
+    }
