c2ff1dbb8df091ac51ed53ef44b65b2bb1454f0e
Zeeshan768 <62187689+Zeeshan768@users.noreply.github.com>
2020-03-29 04:14:05 +0500

    Code Sample 45

---
+++ Code Sample 45 Using Delegates/Code Sample 45/Code Sample 45/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Code_Sample_45
+    {
+    public delegate decimal CalculateFee(decimal balance);
+    public class DelegateDemo
+        {
+        public static decimal RipoffFee(decimal balance)
+            {
+            Console.WriteLine("Calling the Ripoff Method");
+            if(balance < 0)
+                {
+                return 100;
+                }
+            else
+                {
+                return 1;
+                }
+            }
+
+        public static decimal FrindlyFee(decimal balance)
+            {
+            Console.WriteLine("Calling the Friendly Method");
+            if(balance < 0)
+                {
+                return 10;
+                }
+            else
+                {
+                return 1;
+                }
+            }
+        static void Main(string[] args)
+            {
+            CalculateFee calc;
+            calc = new CalculateFee(RipoffFee);
+            calc(-1);
+            calc = new CalculateFee(FrindlyFee);
+            calc(-1);
+            Console.ReadKey();
+            }
+        }
+    }
